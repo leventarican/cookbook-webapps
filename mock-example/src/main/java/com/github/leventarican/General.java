9171dba33cb019993d847de43bf0b8cc6a1e9778
leventarican <levent@protonmail.com>
2019-07-02 23:38:39 +0200

    extends mocks

---
+++ mock-example/src/main/java/com/github/leventarican/General.java
@@ -0,0 +1,11 @@
+package com.github.leventarican;
+
+public class General {
+	public double getPi() {
+		return 3.14;
+	}
+	
+	public int divideZero() {
+		return 0;
+	}
+}
--- mock-example/src/test/java/com/github/leventarican/boundary/DeveloperApiTest.java
+++ mock-example/src/test/java/com/github/leventarican/boundary/DeveloperApiTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import com.github.leventarican.General;
 import com.github.leventarican.control.DeveloperHub;
 import com.github.leventarican.entity.Developer;
 
@@ -15,8 +16,9 @@ public class DeveloperApiTest {
 	@Mock
 	DeveloperHub hub;
 	
+	// inject hub to cut
 	@InjectMocks
-	DeveloperApi api;
+	DeveloperApi cut;	// code / class under test
 	
 	@Before
 	public void setUp() throws Exception {
@@ -24,10 +26,32 @@ public class DeveloperApiTest {
 	}
 	
 	@Test
-	public void test() {
-		Mockito.when(hub.getDeveloper()).thenReturn(new Developer(0L, "java"));		
-		Developer dev = api.getDeveloper();
+	public void testStep0() {
+		// now define the behavior of hub
+		Mockito.when(hub.getDeveloper()).thenReturn(new Developer(0L, "java"));
+		
+		// call
+		Developer dev = cut.getDeveloper();
 		System.out.println(dev);
 	}
 
+	@Test
+	public void testStep1() {
+		General general = Mockito.mock(General.class);
+		Mockito.doReturn(42.0).when(general).getPi();
+		
+		System.out.println(general.getPi());
+		System.out.println(general.getPi());
+		
+		Mockito.verify(general, Mockito.times(2)).getPi();
+	}
+	
+	
+	@Test(expected = ArithmeticException.class)
+	public void testStep2() {
+		General general = Mockito.mock(General.class);
+		Mockito.when(general.divideZero()).thenThrow(new ArithmeticException("i cant divide zero."));
+		general.divideZero();
+	}
+	
 }
