8aa50c39d401eb8d8d0b0e8aa3dd1bde7e9f73c2
leventarican <levent@protonmail.com>
2020-03-14 21:43:52 +0100

    CDI 2, beans and more

---
+++ cdi-example/code-0/src/main/java/com/github/leventarican/App.java
@@ -0,0 +1,10 @@
+package com.github.leventarican;
+
+/**
+ * @author Levent
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println("\t# code-0");
+    }
+}
---
+++ cdi-example/code-1/src/test/java/com/github/leventarican/AppTest.java
@@ -0,0 +1,12 @@
+package com.github.leventarican;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        App.main(null);
+        assertTrue(true);
+    }
+}
